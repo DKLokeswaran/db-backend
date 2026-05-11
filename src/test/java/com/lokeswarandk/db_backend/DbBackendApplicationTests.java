e0917281a5bbd0fdae3acfbd8eae75c9abe69a5a
Lokeswaran DK <lokeswarandk@dkl-home-server.home-server>
2026-05-12 02:20:58 +0530

    Project init

---
+++ src/test/java/com/lokeswarandk/db_backend/DbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lokeswarandk.db_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
