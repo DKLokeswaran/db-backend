c9fd405e5925f47d1f0e7dcb8284dd460aa13375
Lokeswaran DK <lokeswarandk@dkl-home-server.home-server>
2026-05-21 02:06:22 +0530

    added and connected postgresql db

--- src/main/java/com/lokeswarandk/db_backend/DbBackendApplication.java
+++ src/main/java/com/lokeswarandk/db_backend/DbBackendApplication.java
@@ -2,9 +2,10 @@ package com.lokeswarandk.db_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(
+	// exclude = {DataSourceAutoConfiguration.class}
+)
 public class DbBackendApplication {
 
 	public static void main(String[] args) {
